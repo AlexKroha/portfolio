89ff7861fa89295f3910261858dbae57bbe5acea
Krolik <sash2005-3@mail.ru>
2023-08-23 14:43:11 +0500

    Password

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'sash20052@gmail.com'
-EMAIL_HOST_PASSWORD = 'sdivzwxktcanivox'
+EMAIL_HOST_PASSWORD = '***'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
